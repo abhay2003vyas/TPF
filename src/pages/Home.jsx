07e50a2bc441fa3959c130b1f5018ea7cd3dcfbd
Abhay Vyas <vyas0994@gmail.com>
2025-09-26 16:09:29 +0530

    update Hero section for Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,9 @@
 import React from 'react';
 import InstagramGallery from '../components/InstagramGallery';
-
+import Hero from '../components/Hero.jsx';
 function Home() {
   return (
-    <div className="text-center mt-20">
-      <h1 className="text-4xl font-bold text-[#f1b621] mb-4">Welcome to TPF Photography 📸</h1>
-      <p className="text-lg text-gray-700">Capturing moments that last forever.</p>
-
-      {/* Instagram Gallery */}
-      <InstagramGallery />
-    </div>
+      <Hero/>
   );
 }
 
